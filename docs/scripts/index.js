fa713c54b9adf49e4bca62d87a5c65d825531693
Isabel-Lifu-211207-XPrado <ba004745@bac.qld.edu.au>
2021-05-05 19:53:38 +1000

    UPDATE

---
+++ docs/scripts/index.js
@@ -0,0 +1,6 @@
+error = function() {
+	alert("Sorry, this page is currently under construction 🐶 But Prado says it'll complete soon...")
+}
+prado = function() {
+	alert("You are currently in our home page!")
+}
