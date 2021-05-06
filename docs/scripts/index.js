a2d697f7caaf6c953768076a677f8e4903e1d698
TurnipGuy30 <ba004629@bac.qld.edu.au>
2021-05-06 19:52:22 +1000

    Update

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,6 +1,6 @@
 error = function() {
-	alert("Sorry, this page is currently under construction 🐶 But Prado says it'll complete soon...")
-}
+	alert("Sorry, this page is currently under construction 🐶 But Prado says it'll complete soon...");
+};
 prado = function() {
-	alert("You are currently in our home page!")
-}
+	alert("You are currently in our home page!");
+};
