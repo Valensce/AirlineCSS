5d3c637f4b6ad586758e419742e67e222ed0eaac
JohnnySD <ba004629@bac.qld.edu.au>
2021-05-08 21:28:20 +1000

    Update

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,6 +1,8 @@
 error = function() {
-	alert("Sorry, this page is currently under construction 🐶 But Prado says it'll complete soon...");
+	alert("Sorry, this page is currently under construction! 🐶 But Prado says it'll be completed soon...");
 };
 prado = function() {
 	alert("You are currently in our home page!");
 };
+
+// Semicolons added by Johnny (Isabel missed them LOL)
